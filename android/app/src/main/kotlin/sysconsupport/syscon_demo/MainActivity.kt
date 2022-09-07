628370e7dc1c20c54ba5954dc9b4fbceb31b6086
新井寿明 <toshipy@araitoshiakis-MacBook-Pro.local>
2022-09-07 15:07:23 +0900

    test

---
+++ android/app/src/main/kotlin/sysconsupport/syscon_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package sysconsupport.syscon_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ syscon_demo/syscon_demo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  syscon_demo
+//
+//  Created by 新井寿明 on 2022/09/07.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
